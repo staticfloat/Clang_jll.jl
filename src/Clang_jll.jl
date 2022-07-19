d64c3172ba32bd7a2046b5d4f6ff41dcbd962917
jlbuild <juliabuildbot@gmail.com>
2022-07-19 07:50:13 +0000

    Clang_jll build 13.0.1+3

--- src/Clang_jll.jl
+++ src/Clang_jll.jl
@@ -2,7 +2,6 @@
 baremodule Clang_jll
 using Base
 using Base: UUID
-Base.include(@__MODULE__, joinpath("..", ".pkg", "platform_augmentation.jl"))
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("Clang")
