c01e232c91cb68080418582e18e1fec688ab3580
Elliot Saba <staticfloat@gmail.com>
2025-03-02 02:29:55 +0000

    Clang v17.0.7

--- src/Clang_jll.jl
+++ src/Clang_jll.jl
@@ -1,10 +1,5 @@
-# Use baremodule to shave off a few KB from the serialized `.ji` file
-baremodule Clang_jll
-using Base
-using Base: UUID
-Base.include(@__MODULE__, joinpath("..", ".pkg", "platform_augmentation.jl"))
-import JLLWrappers
+module Clang_jll
+using LazyJLLWrappers
+@generate_jll_from_toml()
+end # module Clang_jll
 
-JLLWrappers.@generate_main_file_header("Clang")
-JLLWrappers.@generate_main_file("Clang", UUID("0ee61d77-7f21-5576-8119-9fcc46b10100"))
-end  # module Clang_jll
